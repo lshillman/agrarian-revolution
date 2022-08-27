040b5748e516433a37fee05bcbc06492c6c24d34
Mary Dillon <mary.dillon09@gmail.com>
2022-08-27 10:50:05 -0700

    working frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { getAllMatchups } from '../utils/api';
+// import { Link } from 'react-router-dom';
+// import { getAllMatchups } from '../utils/api';
 
 const Home = () => {
   return (
