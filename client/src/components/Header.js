08254807e5d9888dc11f34366ae8dcbd457332cf
Mary Dillon <mary.dillon09@gmail.com>
2022-09-04 14:01:00 -0700

    creates signup modal

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import Auth from "../utils/auth";
 import { Link } from 'react-router-dom';
 import { Button, Modal } from 'react-bootstrap';
 import LoginForm from '../components/LoginForm';
+import SignupForm from '../components/SignupForm';
 
 export default function Header() {
 
@@ -24,7 +25,7 @@ export default function Header() {
                 </nav>
                 <div id="login">
                     {Auth.loggedIn() && <button id="logout-btn" onClick={logout}>Logout</button>}
-                    {!Auth.loggedIn() && <button id="login-btn"><Link to="/login">Login</Link></button>}
+                    {!Auth.loggedIn() && <button id="signup-btn"onClick={() => setShowModal(true)}>Signup</button>}
                     {!Auth.loggedIn() && <button id="login-btn"onClick={() => setShowModal(true)}>Login</button>}
                     <Modal
                         size='lg'
@@ -37,6 +38,19 @@ export default function Header() {
                         </Modal.Header>
                         <Modal.Body>
                             <LoginForm handleModalClose={() => setShowModal(false)} />
+                        </Modal.Body>
+                    </Modal>
+                    <Modal
+                        size='lg'
+                        show={showModal}
+                        onHide={() => setShowModal(false)}
+                        aria-labelledby='signup-modal'>
+
+                        <Modal.Header closeButton>
+                            <Modal.Title>Signup</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <SignupForm handleModalClose={() => setShowModal(false)} />
                         </Modal.Body>
                     </Modal>
 
