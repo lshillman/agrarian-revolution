e588b77d7ed66e52dfe06ae9c7071d4776bfdc31
lshillman <lshillman@users.noreply.github.com>
2022-09-04 16:57:23 -0700

    add redirect route for unknown paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Search from './pages/Search';
 import MyVeggies from './pages/MyVeggies';
 import Requests from './pages/Requests';
@@ -72,6 +72,10 @@ function App() {
             <Route
               path="/login"
               element={<LoginForm />}
+            />
+            <Route
+              path="/*"
+              element={<Navigate replace to="/" />}
             />
           </Routes>
           <Footer />
