eb6df3eb3335c0b05abcc1db8e00325a6dc7e87e
lshillman <lshillman@users.noreply.github.com>
2022-09-04 22:28:34 -0700

    style requests page

--- client/src/components/IncomingRequests.js
+++ client/src/components/IncomingRequests.js
@@ -12,28 +12,24 @@ export default function IncomingRequests({ veggiesRequests, displayResponses }) 
             <h2>Requests for your veggies</h2>
             {veggiesRequests.map((veggie, i) => {
                 if (veggie.requests.length) {
-                    return <div className="single-request" key={i}>
-                        <div className="single-request-header">
-                            {/* <img src={icons[veggie.type].options.iconUrl} alt="veggie icon" /> */}
-                            {/* <h2>Someone wants your {veggie.type}</h2> */}
-                        </div>
-                        <div>
-                            {/* traverse the user's veggies' requests array */}
-                            {veggie.requests.map((req, i) => (
-                                <>
-                                    <div className="single-response">
+                    return <div className="request-group" key={i}>
+                        {/* traverse the user's veggies' requests array */}
+                        {veggie.requests.map((req, i) => (
+                            <div className="single-request" key={i}>
+                                    
+                                    <div className="request-meta">
                                         <img src={icons[veggie.type].options.iconUrl} alt="veggie icon" />
-                                        <p className="sender-meta"><strong>{req.requestor.username}</strong> <span className="message-timestamp">{moment(req.timestamp).fromNow()}</span></p>
-                                        <p>{req.responses.length ? req.responses[req.responses.length - 1].content : req.content}</p>
+                                        <p className="sender-meta"><strong>{req.requestor.username}</strong><br></br><span className="message-timestamp">{moment(req.timestamp).fromNow()}</span></p>
+                                    
+                                    <p className="snippet">{req.responses.length ? req.responses[req.responses.length - 1].content : req.content}</p>
                                     </div>
-                                    <button className="delete-veggie-btn" id={req._id} onClick={(e) => displayResponses(e)}>
-                                        <Link to={`/requests/${req._id}`}>
-                                            Respond
-                                        </Link>
-                                    </button>
-                                </>
-                            ))}
-                        </div>
+                                <button className="respond-btn" id={req._id} onClick={(e) => displayResponses(e)}>
+                                    <Link to={`/requests/${req._id}`}>
+                                        Respond
+                                    </Link>
+                                </button>
+                            </div>
+                        ))}
                     </div>
                 }
                 return <></>
