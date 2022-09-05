b0d7219e58c5b938511b947427161cd681723383
laurasierra17 <laura.sierra17@gmail.com>
2022-09-05 15:03:30 -0400

    style delete component in my veggies page

--- client/src/components/DeleteBtn.js
+++ client/src/components/DeleteBtn.js
@@ -6,15 +6,23 @@ export default function DeleteBtn({ veggieDelete }) {
         if (auth.loggedIn()) {
             e.currentTarget.style.display = "none";
             e.currentTarget.nextElementSibling.style.display = "block";
+            e.currentTarget.nextElementSibling.nextElementSibling.style.display = "block";
         } else {
             window.location.replace('/');
         }
     }
 
+    const cancelDelete = (e) => {
+        e.currentTarget.style.display = "none";
+        e.currentTarget.previousElementSibling.style.display = "block";
+        e.currentTarget.nextElementSibling.style.display = "none";
+    }
+
     return (
-        <>
+        <div className="delete-actions" style={{ display: "flex", gap: "1rem"}}>
             <button className="delete-veggie-btn" onClick={handleDeleteButton}>Delete</button>
+            <button className="delete-veggie-btn" style={{ display: "none" }} onClick={cancelDelete}>Cancel</button>
             <button className="confirm-delete" style={{ display: 'none', backgroundColor: 'red' }} onClick={veggieDelete} >Confirm</button>
-        </>
+        </div>
     )
 }
