57e9ba57f1036fe27955b8d291641530487cd93a
laurasierra17 <laura.sierra17@gmail.com>
2022-09-05 14:07:40 -0400

    prevent user from posting a veggie if their token expired

--- client/src/components/PostVeggieModal.js
+++ client/src/components/PostVeggieModal.js
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
 import { Button, Modal } from 'react-bootstrap';
 import AddVeggieForm from "./AddVeggieForm";
+import auth from "../utils/auth";
+import { Navigate } from "react-router-dom";
 
 export default function PostVeggieModal() {
     const [showPostModal, setShowPostModal] = useState(false);
 
     return (
         <>
-            <Modal
-                size='lg'
-                show={showPostModal}
-                onHide={() => setShowPostModal(false)}
-                aria-labelledby='veggie-modal'>
+            {auth.loggedIn() ? (
+                <>
+                    <Modal
+                        size='lg'
+                        show={showPostModal}
+                        onHide={() => setShowPostModal(false)}
+                        aria-labelledby='veggie-modal'>
 
-                <Modal.Header closeButton>
-                    <Modal.Title>Post a Veggie</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <AddVeggieForm handleModalClose={() => setShowPostModal(false)} />
-                </Modal.Body>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Post a Veggie</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <AddVeggieForm handleModalClose={() => setShowPostModal(false)} />
+                        </Modal.Body>
 
-            </Modal>
+                    </Modal>
 
-            <Button onClick={() => setShowPostModal(true)}>
-                Post Veggie
-            </Button>
+                    <Button onClick={() => setShowPostModal(true)}>
+                        Post Veggie
+                    </Button>
+                </>
+            ) : window.location.replace('/') }
         </>
     )
 }
