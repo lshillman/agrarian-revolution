a6521090aafdea01b7e87df8a878335054769dc4
lshillman <lshillman@users.noreply.github.com>
2022-10-03 16:52:06 -0700

    create associations for SQL models

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,42 @@ const User = require('./User');
 const Veggie = require('./Veggie');
 const Request = require('./Request');
 
+User.hasMany(Veggie, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Veggie.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Veggie.hasMany(Request, {
+    foreignKey: 'veggie_id',
+    onDelete: 'CASCADE'
+});
+
+Request.belongsTo(Veggie, {
+    foreignKey: 'veggie_id'
+});
+
+User.hasMany(Request, {
+    foreignKey: 'requestor_id',
+    onDelete: 'SET NULL'
+});
+
+User.hasMany(Request, {
+    foreignKey: 'owner_id',
+    onDelete: 'SET NULL'
+});
+
+Request.belongsTo(User, {
+    foreignKey: 'requestor_id'
+});
+
+Request.belongsTo(User, {
+    foreignKey: 'owner_id'
+});
+
+
+
 module.exports = { User, Veggie, Request };
